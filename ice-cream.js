bda6066e9827b993d71ed2a6d3715b0656c06bb5
thomaswyee <thomas.w.yee@gmail.com>
2020-12-06 17:58:41 +0000

    push all

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+const iceCreamFlavours = [
+  'chocolate',
+  'vanilla',
+  'cookies and cream',
+  'rocky road',
+  'strawberry',
+];
+console.log(iceCreamFlavours);
+const arr = [...iceCreamFlavours, 'root beer'];
+console.log(arr[0]);
+console.log(arr[arr.length - 1]);
--- test.js
+++ test.js
@@ -1,18 +1,57 @@
-for (let i = 100; i <= 200; i++) {
-  // if (i % 3 === 0 && i % 4 === 0) {
-  //   console.log('LoopyLighthouse');
-  // } else if (i % 3 === 0) {
-  //   console.log('Loopy');
-  // } else if (i % 4 === 0) {
-  //   console.log('Lighthouse');
-  // } else {
-  //   console.log(i);
-  // }
-  i % 3 === 0 && i % 4 === 0
-    ? console.log('LoopyLighthouse')
-    : i % 3 === 0
-    ? console.log('Loopy')
-    : i % 4 === 0
-    ? console.log('Lighthouse')
-    : console.log(i);
-}
+// const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north'];
+
+// const finalPosition = function (moves) {
+//   let p = [0, 0];
+//   moves.map((item) =>
+//     item === 'north'
+//       ? p[1]++
+//       : item === 'south'
+//       ? p[1]--
+//       : item === 'east'
+//       ? p[0]++
+//       : p[0]--
+//   );
+//   return p;
+// };
+
+// console.log(finalPosition(moves));
+
+// const ageCalculator = function (name, yearOfBirth, currentYear) {
+//   return `${name} is ${currentYear - yearOfBirth} years old.`;
+// };
+// console.log(ageCalculator('Suzie', 1983, 2015));
+// console.log(ageCalculator('Ferdinand', 1988, 2015));
+
+// const howManyHundreds = (num) => {
+//   return num < 100
+//     ? 0
+//     : num % 100 === 0
+//     ? num / 100
+//     : (num - (num % 100)) / 100;
+// };
+// console.log(howManyHundreds(1000));
+// console.log(howManyHundreds(894));
+// console.log(howManyHundreds(520));
+// console.log(howManyHundreds(99));
+// console.log(howManyHundreds(0));
+
+const calculateRectangleArea = (length, width) => {
+  return length < 0 || width < 0 ? undefined : length * width;
+};
+const calculateTriangleArea = (base, height) => {
+  return base < 0 || height < 0 ? undefined : (base * height) / 2;
+};
+const calculateCircleArea = (radius) => {
+  return radius < 0 ? undefined : Math.PI * (radius * radius);
+};
+console.log(calculateRectangleArea(10, 5)); // should print 50
+console.log(calculateRectangleArea(1.5, 2.5)); // should print 3.75
+console.log(calculateRectangleArea(10, -5)); // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1));
