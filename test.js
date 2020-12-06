b347f0c8408bc0b13f2e24d9bc8be3b1e01a804a
thomaswyee <thomas.w.yee@gmail.com>
2020-12-06 19:51:11 +0000

    Object and Function update. I alreay finished before, this is just an update of the test.js

--- test.js
+++ test.js
@@ -35,23 +35,94 @@
 // console.log(howManyHundreds(99));
 // console.log(howManyHundreds(0));
 
-const calculateRectangleArea = (length, width) => {
-  return length < 0 || width < 0 ? undefined : length * width;
-};
-const calculateTriangleArea = (base, height) => {
-  return base < 0 || height < 0 ? undefined : (base * height) / 2;
-};
-const calculateCircleArea = (radius) => {
-  return radius < 0 ? undefined : Math.PI * (radius * radius);
-};
-console.log(calculateRectangleArea(10, 5)); // should print 50
-console.log(calculateRectangleArea(1.5, 2.5)); // should print 3.75
-console.log(calculateRectangleArea(10, -5)); // should print undefined
+// const calculateRectangleArea = (length, width) => {
+//   return length < 0 || width < 0 ? undefined : length * width;
+// };
+// const calculateTriangleArea = (base, height) => {
+//   return base < 0 || height < 0 ? undefined : (base * height) / 2;
+// };
+// const calculateCircleArea = (radius) => {
+//   return radius < 0 ? undefined : Math.PI * (radius * radius);
+// };
+// console.log(calculateRectangleArea(10, 5)); // should print 50
+// console.log(calculateRectangleArea(1.5, 2.5)); // should print 3.75
+// console.log(calculateRectangleArea(10, -5)); // should print undefined
 
-console.log(calculateTriangleArea(10, 5)); // should print 25
-console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
-console.log(calculateTriangleArea(10, -5)); // should print undefined
+// console.log(calculateTriangleArea(10, 5)); // should print 25
+// console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+// console.log(calculateTriangleArea(10, -5)); // should print undefined
 
-console.log(calculateCircleArea(10)); // should print 314.159...
-console.log(calculateCircleArea(3.5)); // should print 38.484...
-console.log(calculateCircleArea(-1));
+// console.log(calculateCircleArea(10)); // should print 314.159...
+// console.log(calculateCircleArea(3.5)); // should print 38.484...
+// console.log(calculateCircleArea(-1));
+
+// const range = (start, end, step) => {
+//   let arr = [];
+//   if (
+//     start === undefined ||
+//     end === undefined ||
+//     step === undefined ||
+//     start > end ||
+//     step <= 0
+//   ) {
+//     return arr;
+//   } else {
+//     for (let i = start; i <= end; i += step) {
+//       arr.push(i);
+//     }
+//     return arr;
+//   }
+// };
+
+// console.log(range(0, 10, 2));
+// console.log(range(10, 30, 5));
+// console.log(range(-5, 2, 3));
+
+// const lastIndexOf = (arr, num) => {
+//   let len = arr.length - 1;
+//   for (let i = len; i >= 0; i--) {
+//     if (arr[i] === num) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// };
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 1));
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 1), '=?', 3);
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 2), '=?', 4);
+// console.log(lastIndexOf([0, 1, 4, 1, 2], 3), '=?', -1);
+// console.log(lastIndexOf([5, 5, 5], 5), '=?', 2);
+// console.log(lastIndexOf([], 3), '=?', -1);
+
+// const loopyLighthouse = (range, multiples, words) => {
+//   for (let i = range[0]; i <= range[1]; i++) {
+//     console.log(
+//       i % multiples[0] === 0 && i % multiples[1] === 0
+//         ? `${words[0]}${words[1]}`
+//         : i % multiples[0] === 0
+//         ? `${words[0]}`
+//         : i % multiples[1] === 0
+//         ? `${words[1]}`
+//         : i
+//     );
+//   }
+// };
+// loopyLighthouse([15, 90], [2, 5], ['Batty', 'Beacon']);
+
+// const concat = (arr1, arr2) => {
+//   return [...arr1, ...arr2];
+// };
+// console.log(concat([1, 2, 3], [4, 5, 6]));
+// console.log(concat([1, 2, 3], [4, 5, 6]), '=?', [1, 2, 3, 4, 5, 6]);
+// console.log(concat([0, 3, 1], [9, 7, 2]), '=?', [0, 3, 1, 9, 7, 2]);
+// console.log(concat([], [9, 7, 2]), '=?', [9, 7, 2]);
+// console.log(concat([5, 10], []), '=?', [5, 10]);
+
+const merge = (arr1, arr2) => {
+  let newArr = [...arr1, ...arr2];
+  return newArr.sort((a, b) => {
+    return a - b;
+  });
+};
+
+console.log(merge([4, 5, 6], [1, 2, 3, 4]));
